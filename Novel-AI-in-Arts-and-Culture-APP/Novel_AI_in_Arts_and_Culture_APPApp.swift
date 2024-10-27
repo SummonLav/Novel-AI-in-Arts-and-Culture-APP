40442c10e709872c38c6191d0ea00ec6395e26ab
noirshan_ <79836722+SummonLav@users.noreply.github.com>
2024-10-26 22:10:55 -0400

    Add files via upload

---
+++ Novel-AI-in-Arts-and-Culture-APP/Novel_AI_in_Arts_and_Culture_APPApp.swift
@@ -0,0 +1,17 @@
+//
+//  Novel_AI_in_Arts_and_Culture_APPApp.swift
+//  Novel-AI-in-Arts-and-Culture-APP
+//
+//  Created by Lavenda Shan on 10/16/24.
+//
+
+import SwiftUI
+
+@main
+struct Novel_AI_in_Arts_and_Culture_APPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            AIChatbotView()
+        }
+    }
+}
